e74e80f70b96b984b647e5f15ab14ab825bbc2e7
fran-itria <francoitria01@gmail.com>
2026-09-02 03:28:16 -0300

    Add constants for API endpoints and color mappings

---
+++ js/Const/const.js
@@ -0,0 +1,31 @@
+export const API_BASE_PRODUCCION = "https://matesparana-backend-production.up.railway.app";
+export const API_BASE_PRUEBA = "http://localhost:3000";
+
+
+//  Credenciales de Payway
+export const PAYWAY_API_KEY_PRODUCCION = "0263f55d7bf6464cabd58c904e1d258d";
+export const PAYWAY_URL_PRODUCCION = "https://ventasonline.payway.com.ar/api/v2";
+
+export const PAYWAY_API_KEY_SANDBOX = "e9cdb99fff374b5f91da4480c8dca741";
+export const PAYWAY_URL_SANDBOX = "https://developers.decidir.com/api/v2";
+
+// Colores
+
+export const colorMap = {
+    negro: "#000",
+    blanco: "#fff",
+    rojo: "#c00",
+    rojoOscuro: "rgb(112, 7, 7)",
+    rosa: "rgb(233, 152, 152)",
+    verde: "#477e4c",
+    azul: "#34449b",
+    amarillo: "#ffd23d",
+    violeta: "#773dff",
+    gris: "#757575",
+    marron: "#553321",
+    chocolate: "#4d2e1e",
+    marronclaro: "#e08f64",
+    marronoscuro: "#442617",
+    borravino: "#5e2231",
+    beige: "#d2b48c",
+};
